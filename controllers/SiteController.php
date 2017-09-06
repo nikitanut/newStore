08b37c750a9901c2911e942a838f6a8ea5834067
Nikita Nuttunen <nikitanut@gmail.com>
2017-09-06 23:03:52 +0300

    first commit

---
+++ controllers/SiteController.php
@@ -0,0 +1,25 @@
+<?php
+
+//include_once ROOT . '/models/Category.php';
+//include_once ROOT . '/models/Product.php';
+
+class SiteController
+{
+
+    public function actionIndex()
+    {
+        /*$categories = array();
+        $categories = Category::getCategoriesList();
+
+        $latestProducts = array();
+        $latestProducts = Product::getLatestProducts(6);
+        
+        $recomendedProducts = array();
+        $recomendedProducts = Product::getRecommendedProducts();
+        */
+        require_once(ROOT . '/views/site/index.php');
+
+        return true;
+    }
+
+}
