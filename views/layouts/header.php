603b3821baf5ff141a4c14979872b9e5aaa94197
Nikita <nikitanut@gmail.com>
2017-09-07 21:02:49 +0300

    add header, footer

---
+++ views/layouts/header.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Центр проката г. Нарьян-Мар</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet"/>
+<link href="template/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="template/js/jquery.min.js"></script>	
+<link href="template/css/slider.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+<body>
+<!-- start header -->
+<div class="header_bg">
+<div class="wrap">
+	<div class="header">
+		<div class="logo">
+			<a href="index.php"><img src="template/images/logo.png" alt=""/> </a>
+		</div>
+		<div class="h_icon">
+		<ul class="icon1 sub-icon1">
+			<li><a class="active-icon c1" href="#"><i>$300</i></a>
+				<ul class="sub-icon1 list">
+					<li><h3>shopping cart empty</h3><a href=""></a></li>
+					<li><p>if items in your wishlit are missing, <a href="new/contact.html">contact us</a> to view them</p></li>
+				</ul>
+			</li>
+		</ul>
+		</div>
+		<div class="h_search">
+    		<form>
+    			<input type="text" value="">
+    			<input type="submit" value="">
+    		</form>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+</div>
+<div class="header_btm">
+<div class="wrap">
+	<div class="header_sub">
+		<div class="h_menu">
+			<ul>
+				<li class="active"><a href="#">Главная</a></li> |
+                                <li><a href="#">Каталог</a></li> |
+                                <li><a href="#">Контакты</a></li>
+				<!--
+                                <li><a href="#">Игровые комплексы, горки, качели</a></li> |
+                                <li><a href="#">Батуты</a></li> |
+                                <li><a href="#">Каталки, прыгуны, поницыкл</a></li> |
+                                <li><a href="#">Ходунки</a></li> |
+                                <li><a href="#">Детские центры, шезлонги, качели</a></li> |
+                                <li><a href="#">Игры</a></li> |
+                                <li><a href="#">Для праздника</a></li> |
+                                <li><a href="#">Развивающие игрушки</a></li> |
+                                <li><a href="#">Крупные игрушки</a></li> |
+                                <li><a href="#">Сопутствующие товары</a></li> |
+                                 -->
+                                
+                           <!-- 
+                            <li class="active"><a href="index.php">Home</a></li> |
+				<li><a href="new/sale.html">sale</a></li> |
+				<li><a href="new/handbags.html">handbags</a></li> |
+				<li><a href="new/accessories.html">accessories</a></li> |
+				<li><a href="new/wallets.html">wallets</a></li> |
+				<li><a href="new/sale.html">sale</a></li> |
+				<li><a href="index.php">mens store</a></li> |
+				<li><a href="new/shoes.html">shoes</a></li> |
+				<li><a href="new/sale.html">vintage</a></li> |
+				<li><a href="new/service.html">services</a></li> |
+				<li><a href="new/contact.html">Contact us</a></li>
+                             -->
+			</ul>
+		</div>
+		<div class="top-nav">
+	          <nav class="nav">	        	
+	    	    <a href="#" id="w3-menu-trigger"> </a>
+	                  <ul class="nav-list" style="">
+	            	        <li class="nav-item"><a class="active" href="index.php">Home</a></li>
+							<li class="nav-item"><a href="new/sale.html">Sale</a></li>
+							<li class="nav-item"><a href="new/handbags.html">Handbags</a></li>
+							<li class="nav-item"><a href="new/accessories.html">Accessories</a></li>
+							<li class="nav-item"><a href="new/shoes.html">Shoes</a></li>
+							<li class="nav-item"><a href="new/service.html">Services</a></li>
+							<li class="nav-item"><a href="new/contact.html">Contact</a></li>
+	                 </ul>
+	           </nav>
+	             <div class="search_box">
+				<form>
+				   <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}"><input type="submit" value="">
+			    </form>
+			</div>
+	          <div class="clear"> </div>
+	          <script src="template/js/responsive.menu.js"></script>
+         </div>		  
+	<div class="clear"></div>
+</div>
+</div>
+</div>
--- views/site/index.php
+++ views/site/index.php
@@ -4,15 +4,8 @@ Author URL: http://w3layouts.com
 License: Creative Commons Attribution 3.0 Unported
 License URL: http://creativecommons.org/licenses/by/3.0/
 -->
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Free Aditii Website Template | Home :: w3layouts</title>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
-<link href="template/css/style.css" rel="stylesheet" type="text/css" media="all" />
-<script type="text/javascript" src="template/js/jquery.min.js"></script>
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
 <!-- start slider -->		
 	<link href="template/css/slider.css" rel="stylesheet" type="text/css" media="all" />
 	<script type="text/javascript" src="template/js/modernizr.custom.28468.js"></script>
@@ -56,78 +49,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					});
 				});
 			</script>
-</head>
-<body>
-<!-- start header -->
-<div class="header_bg">
-<div class="wrap">
-	<div class="header">
-		<div class="logo">
-			<a href="index.php"><img src="template/images/logo.png" alt=""/> </a>
-		</div>
-		<div class="h_icon">
-		<ul class="icon1 sub-icon1">
-			<li><a class="active-icon c1" href="#"><i>$300</i></a>
-				<ul class="sub-icon1 list">
-					<li><h3>shopping cart empty</h3><a href=""></a></li>
-					<li><p>if items in your wishlit are missing, <a href="new/contact.html">contact us</a> to view them</p></li>
-				</ul>
-			</li>
-		</ul>
-		</div>
-		<div class="h_search">
-    		<form>
-    			<input type="text" value="">
-    			<input type="submit" value="">
-    		</form>
-		</div>
-		<div class="clear"></div>
-	</div>
-</div>
-</div>
-<div class="header_btm">
-<div class="wrap">
-	<div class="header_sub">
-		<div class="h_menu">
-			<ul>
-				<li class="active"><a href="index.php">Home</a></li> |
-				<li><a href="new/sale.html">sale</a></li> |
-				<li><a href="new/handbags.html">handbags</a></li> |
-				<li><a href="new/accessories.html">accessories</a></li> |
-				<li><a href="new/wallets.html">wallets</a></li> |
-				<li><a href="new/sale.html">sale</a></li> |
-				<li><a href="index.php">mens store</a></li> |
-				<li><a href="new/shoes.html">shoes</a></li> |
-				<li><a href="new/sale.html">vintage</a></li> |
-				<li><a href="new/service.html">services</a></li> |
-				<li><a href="new/contact.html">Contact us</a></li>
-			</ul>
-		</div>
-		<div class="top-nav">
-	          <nav class="nav">	        	
-	    	    <a href="#" id="w3-menu-trigger"> </a>
-	                  <ul class="nav-list" style="">
-	            	        <li class="nav-item"><a class="active" href="index.php">Home</a></li>
-							<li class="nav-item"><a href="new/sale.html">Sale</a></li>
-							<li class="nav-item"><a href="new/handbags.html">Handbags</a></li>
-							<li class="nav-item"><a href="new/accessories.html">Accessories</a></li>
-							<li class="nav-item"><a href="new/shoes.html">Shoes</a></li>
-							<li class="nav-item"><a href="new/service.html">Services</a></li>
-							<li class="nav-item"><a href="new/contact.html">Contact</a></li>
-	                 </ul>
-	           </nav>
-	             <div class="search_box">
-				<form>
-				   <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}"><input type="submit" value="">
-			    </form>
-			</div>
-	          <div class="clear"> </div>
-	          <script src="template/js/responsive.menu.js"></script>
-         </div>		  
-	<div class="clear"></div>
-</div>
-</div>
-</div>
+
+
+
 <!-- start slider -->
 			<div id="da-slider" class="da-slider">
 				<div class="da-slide">
@@ -313,97 +237,4 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	</div>
 </div>
 </div>	
-<!-- start footer -->
-<div class="footer_bg">
-<div class="wrap">	
-	<div class="footer">
-		<!-- start grids_of_4 -->	
-		<div class="grids_of_4">
-			<div class="grid1_of_4">
-				<h4>featured sale</h4>
-				<ul class="f_nav">
-					<li><a href="">alexis Hudson</a></li>
-					<li><a href="">american apparel</a></li>
-					<li><a href="">ben sherman</a></li>
-					<li><a href="">big buddha</a></li>
-					<li><a href="">channel</a></li>
-					<li><a href="">christian audigier</a></li>
-					<li><a href="">coach</a></li>
-					<li><a href="">cole haan</a></li>
-				</ul>
-			</div>
-			<div class="grid1_of_4">
-				<h4>mens store</h4>
-				<ul class="f_nav">
-					<li><a href="">alexis Hudson</a></li>
-					<li><a href="">american apparel</a></li>
-					<li><a href="">ben sherman</a></li>
-					<li><a href="">big buddha</a></li>
-					<li><a href="">channel</a></li>
-					<li><a href="">christian audigier</a></li>
-					<li><a href="">coach</a></li>
-					<li><a href="">cole haan</a></li>
-				</ul>
-			</div>
-			<div class="grid1_of_4">
-				<h4>women store</h4>
-				<ul class="f_nav">
-					<li><a href="">alexis Hudson</a></li>
-					<li><a href="">american apparel</a></li>
-					<li><a href="">ben sherman</a></li>
-					<li><a href="">big buddha</a></li>
-					<li><a href="">channel</a></li>
-					<li><a href="">christian audigier</a></li>
-					<li><a href="">coach</a></li>
-					<li><a href="">cole haan</a></li>
-				</ul>
-			</div>
-			<div class="grid1_of_4">
-				<h4>quick links</h4>
-				<ul class="f_nav">
-					<li><a href="">alexis Hudson</a></li>
-					<li><a href="">american apparel</a></li>
-					<li><a href="">ben sherman</a></li>
-					<li><a href="">big buddha</a></li>
-					<li><a href="">channel</a></li>
-					<li><a href="">christian audigier</a></li>
-					<li><a href="">coach</a></li>
-					<li><a href="">cole haan</a></li>
-				</ul>
-			</div>
-			<div class="clear"></div>
-		</div>
-	</div>
-</div>
-</div>	
-<!-- start footer -->
-<div class="footer_bg1">
-<div class="wrap">
-	<div class="footer">
-		<!-- scroll_top_btn -->
-	    <script type="text/javascript">
-			$(document).ready(function() {
-			
-				var defaults = {
-		  			containerID: 'toTop', // fading element id
-					containerHoverID: 'toTopHover', // fading element hover id
-					scrollSpeed: 1200,
-					easingType: 'linear' 
-		 		};
-				
-				
-				$().UItoTop({ easingType: 'easeOutQuart' });
-				
-			});
-		</script>
-		 <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
-		<!--end scroll_top_btn -->
-		<div class="copy">
-			<p class="link">&copy;  All rights reserved | Template by&nbsp;&nbsp;<a href="http://w3layouts.com/"> W3Layouts</a></p>
-		</div>
-		<div class="clear"></div>
-	</div>
-</div>
-</div>
-</body>
-</html>
+<?php include ROOT . '/views/layouts/footer.php';
